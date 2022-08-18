162fba5475ed64927c095b694e80d42b325a8af2
samtech85 <sbonheur85@gmail.com>
2022-08-18 13:07:58 -0800

    This the first part of an interactive calculator project

---
+++ javaCalculator/src/addition/Calculator.java
@@ -0,0 +1,15 @@
+package addition;
+
+import javax.swing.JOptionPane;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Addition add = new Addition();
+				
+		 JOptionPane.showMessageDialog(null,"The result is \n" + add.total);
+
+	}
+
+}
